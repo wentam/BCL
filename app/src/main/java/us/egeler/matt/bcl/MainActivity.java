0d22523146b598259f4d43f09f60da7185f0977b
wentam <wentam42@gmail.com>
2019-05-06 00:10:26 -0400

    fully working!

--- app/src/main/java/us/egeler/matt/bcl/MainActivity.java
+++ app/src/main/java/us/egeler/matt/bcl/MainActivity.java
@@ -1,7 +1,11 @@
 package us.egeler.matt.bcl;
 
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.provider.Settings;
@@ -13,12 +17,16 @@ import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    ArrayList<String[]> launchers;
+    private int selectedIndex = 0;
+    private boolean ignoreInput = false;
 
     private void setScreenBrightness(int brightness) {
         //Get the content resolver
@@ -40,20 +48,137 @@ public class MainActivity extends AppCompatActivity {
         window.setAttributes(layoutpars);
     }
 
+
+    private void handleKeyPress(String action) {
+        if (action.equals("power_button.long_pressed")) {
+            Log.d("BCL","Power button long press, trying to shut down...");
+            Intent intent=new Intent("com.wahoofitness.bolt.system.shutdown");
+            sendBroadcast(intent);
+        }
+
+        if (action.equals("up_button.pressed")) {
+            launchers.get(selectedIndex)[3] = "0";
+            selectedIndex--;
+            if (selectedIndex < 0) {
+                selectedIndex = 0;
+            }
+            launchers.get(selectedIndex)[3] = "1";
+            mAdapter.notifyDataSetChanged();
+        }
+
+        if (action.equals("down_button.pressed")) {
+            launchers.get(selectedIndex)[3] = "0";
+            selectedIndex++;
+            if (selectedIndex > (launchers.size()-1)) {
+                selectedIndex = launchers.size()-1;
+            }
+            launchers.get(selectedIndex)[3] = "1";
+
+            mAdapter.notifyDataSetChanged();
+        }
+
+        if (action.equals("center_button.pressed")) {
+            // launch based on selectedIndex
+            String activityToLaunch = launchers.get(selectedIndex)[0];
+
+                Intent intent = new Intent();
+                Log.d("BCL",launchers.get(selectedIndex)[2]);
+                Log.d("BCL",activityToLaunch);
+                intent.setClassName(launchers.get(selectedIndex)[2],activityToLaunch);
+                startActivity(intent);
+        }
+    }
+
+
+    private void startKeyPressListener() {
+        // create broadcastReceiver to listen for button presses
+        IntentFilter filter = new IntentFilter("com.wahoofitness.bolt.buttons.center_button.pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.center_button.down");
+        filter.addAction("com.wahoofitness.bolt.buttons.center_button.up");
+        filter.addAction("com.wahoofitness.bolt.buttons.center_button.long_prossed");
+        filter.addAction("com.wahoofitness.bolt.buttons.left_button.down");
+        filter.addAction("com.wahoofitness.bolt.buttons.left_button.up");
+        filter.addAction("com.wahoofitness.bolt.buttons.left_button.pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.left_button.long_pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.right_button.down");
+        filter.addAction("com.wahoofitness.bolt.buttons.right_button.up");
+        filter.addAction("com.wahoofitness.bolt.buttons.right_button.pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.right_button.long_pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.up_button.down");
+        filter.addAction("com.wahoofitness.bolt.buttons.up_button.up");
+        filter.addAction("com.wahoofitness.bolt.buttons.up_button.pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.up_button.long_pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.down_button.down");
+        filter.addAction("com.wahoofitness.bolt.buttons.down_button.up");
+        filter.addAction("com.wahoofitness.bolt.buttons.down_button.pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.down_button.long_pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.power_button.down");
+        filter.addAction("com.wahoofitness.bolt.buttons.power_button.up");
+        filter.addAction("com.wahoofitness.bolt.buttons.power_button.pressed");
+        filter.addAction("com.wahoofitness.bolt.buttons.power_button.long_pressed");
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (ignoreInput == false) {
+                    handleKeyPress(intent.getAction().split("\\.")[4] + "." + intent.getAction().split("\\.")[5]);
+                }
+            }
+        };
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        ignoreInput = true;
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        ignoreInput = false;
+        setScreenBrightness(10);
+        super.onResume();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setScreenBrightness(10);
+        startKeyPressListener();
 
         // get a list of launchers available
+        launchers = new ArrayList<String[]>();
+
         PackageManager pm = getPackageManager();
         Intent i = new Intent(Intent.ACTION_MAIN);
         i.addCategory(Intent.CATEGORY_HOME);
         List<ResolveInfo> lst = pm.queryIntentActivities(i, 0);
+        int it = 0;
         for (ResolveInfo resolveInfo : lst) {
-            Log.d("BCL", "New Launcher Found: " + resolveInfo.activityInfo.packageName);
+            ApplicationInfo app = null;
+
+            try {
+                app = this.getPackageManager().getApplicationInfo(resolveInfo.activityInfo.packageName, 0);
+            } catch (Exception e) {
+                Log.e("OBC", ""+e);
+            }
+
+            String appName = (String) this.getPackageManager().getApplicationLabel(app);
+
+            if (resolveInfo.activityInfo.packageName.equals("com.wahoofitness.boltlauncher")) {
+                appName = "Official wahoo app";
+            }
+
+            if (!(resolveInfo.activityInfo.packageName.equals("us.egeler.matt.bcl"))) {
+                launchers.add(new String[4]);
+                String[] s = {resolveInfo.activityInfo.name, appName, resolveInfo.activityInfo.packageName,"0"};
+                launchers.set(it, s);
+                it++;
+            }
+
         }
 
         recyclerView = (RecyclerView) findViewById(R.id.rview);
@@ -63,8 +188,8 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         // specify an adapter (see also next example)
-        String []data = {"elem 1", "elem 2"};
-        mAdapter = new menuAdapter(data);
+        launchers.get(selectedIndex)[3] = "1";
+        mAdapter = new menuAdapter(launchers);
         recyclerView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/us/egeler/matt/bcl/menuAdapter.java
+++ app/src/main/java/us/egeler/matt/bcl/menuAdapter.java
@@ -1,29 +1,33 @@
 package us.egeler.matt.bcl;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class menuAdapter extends  RecyclerView.Adapter<menuAdapter.MyViewHolder> {
-    private String[] mDataset;
+    ArrayList<String[]> launchers = new ArrayList<String[]>();
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView textView;
-        public MyViewHolder(TextView v) {
-            super(v);
-            textView = v;
+        public ConstraintLayout layout;
+        public MyViewHolder(ConstraintLayout l) {
+            super(l);
+            layout = l;
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public menuAdapter(String[] myDataset) {
-        mDataset = myDataset;
+    public menuAdapter(ArrayList<String[]> myDataset) {
+        launchers = myDataset;
     }
 
     // Create new views (invoked by the layout manager)
@@ -31,9 +35,14 @@ public class menuAdapter extends  RecyclerView.Adapter<menuAdapter.MyViewHolder>
     public menuAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
         // create a new view
-        TextView v = new TextView(parent.getContext());
-        v.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35f);
-        MyViewHolder vh = new MyViewHolder(v);
+        ConstraintLayout l = (ConstraintLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_item, parent, false);
+        TextView v = l.findViewById(R.id.textView1);
+        v.getPaint().setAntiAlias(false);
+
+        TextView v2 = l.findViewById(R.id.textView2);
+        v2.getPaint().setAntiAlias(false);
+
+        MyViewHolder vh = new MyViewHolder(l);
         return vh;
     }
 
@@ -42,13 +51,25 @@ public class menuAdapter extends  RecyclerView.Adapter<menuAdapter.MyViewHolder>
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
+        ConstraintLayout l = holder.layout;
 
+        ((TextView) l.getChildAt(0)).setText(launchers.get(position)[1]);
+        ((TextView) l.getChildAt(1)).setText(launchers.get(position)[0]);
+
+        if(launchers.get(position)[3].equals("1")) {
+            l.setBackgroundColor(0xFF000000);
+            ((TextView) l.getChildAt(0)).setTextColor(0xFFFFFFFF);
+            ((TextView) l.getChildAt(1)).setTextColor(0xFFFFFFFF);
+        } else {
+            l.setBackgroundColor(0xFFFFFFFF);
+            ((TextView) l.getChildAt(0)).setTextColor(0xFF000000);
+            ((TextView) l.getChildAt(1)).setTextColor(0xFF000000);
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return launchers.size();
     }
 }
